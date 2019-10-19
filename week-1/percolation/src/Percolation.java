c2eb84299eece2bd9fe01aaf82e056e3c0954421
Larry Botha <larry@fixate.it>
2019-10-19 00:23:30 -0500

    add interface

--- week-1/percolation/src/Percolation.java
+++ week-1/percolation/src/Percolation.java
@@ -3,7 +3,38 @@ import edu.princeton.cs.algs4.StdStats;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
-  public static void main(String args[]) {
-    System.out.println("Hello World");
+  // creates n-by-n grid, with all sites initially blocked
+  public Percolation(int n) {
+
+  }
+
+  // opens the site (row, col) if it is not open already
+  public void open(int row, int col) {
+
+  }
+
+  // is the site (row, col) open?
+  public boolean isOpen(int row, int col) {
+    return false;
+  }
+
+  // is the site (row, col) full?
+  public boolean isFull(int row, int col) {
+    return false;
+  }
+
+  // returns the number of open sites
+  public int numberOfOpenSites() {
+    return 0;
+  }
+
+  // does the system percolate?
+  public boolean percolates() {
+    return false;
+  }
+
+  // test client (optional)
+  public static void main(String[] args) {
+
   }
 }
