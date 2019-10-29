f7e69c39d1ca0625e217c77d31d6231f8224f1e1
Larry Botha <larry@fixate.it>
2019-10-28 23:03:20 -0500

    create union

--- week-1/percolation-js/index.js
+++ week-1/percolation-js/index.js
@@ -3,10 +3,30 @@ const {quickUnionWPc} = require('../quick-union-weighted-path-compression');
 const percolation = n => {
   const grid = [];
   const ids = [];
+  let qu;
 
   for (let i = 0; i < n ** 2; i++) {
     ids[i] = i;
     grid[i] = false;
+  }
+
+  /**
+   * create union with an additional node before and after our grid
+   */
+  qu = quickUnionWPc(n ** 2 + 2);
+
+  /**
+   * connect the first 10 grid items to the 0 node
+   */
+  for (let i = 1; i < n + 1; i++) {
+    qu.union(0, i);
+  }
+
+  /**
+   * connect the last 10 grid items to the 0 node
+   */
+  for (let i = n ** 2 - n + 1; i < n ** 2 + 1; i++) {
+    qu.union(i, n ** 2 + 1);
   }
 
   const getId = (row, col) => (row - 1) * (n - 1) + col;
@@ -48,6 +68,7 @@ const percolation = n => {
     numberOfOpenSites,
     open,
     percolates,
+    qu,
   };
 };
 
