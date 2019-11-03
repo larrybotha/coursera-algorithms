320b73445fd7f8e0afae82e0274dc3a013a0034f
Larry Botha <larry@fixate.it>
2019-11-03 10:52:30 -0500

    fix and annotate algorithm

--- week-01/02-analysis-of-algorithms/3-sum-brute-force.js
+++ week-01/02-analysis-of-algorithms/3-sum-brute-force.js
@@ -1,11 +1,22 @@
-// threeSum :: int[] -> int
+/**
+ * threeSum
+ *
+ * threeSum :: int[] -> int
+ *
+ * @param xs - an array of discrete integers
+ * @returns count - number of combinations of 3 numbers that sum to 0
+ */
 const threeSum = xs => {
   const {length} = xs;
   let count = 0;
 
+  // loop over every item
   for (let i = 0; i < length; i++) {
-    for (let j = 0; j < length; j++) {
-      for (let k = 0; k < length; k++) {
+    // getting every item after that item
+    for (let j = i + 1; j < length; j++) {
+      // and every item after that item
+      for (let k = j + 1; k < length; k++) {
+        // summing them
         const sum = xs[i] + xs[j] + xs[k];
 
         if (sum === 0) {
